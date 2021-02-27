647e17638ba1e2b3447666b08f2d496b7029da12
Lapo Rastrelli <lr4617@ic.ac.uk>
2021-02-27 23:47:00 +0100

    Pulling

--- Homework.m
+++ Homework.m
@@ -23,7 +23,7 @@ scatter(col, row, 2, 'filled')
 
 
 %% PSTH
-trial_n = 1; %trial you want to observe
+trial_n = 100; %trial you want to observe
 angle_n = 1; %reaching angle you want ot observe
 bin_n = length(trial(trial_n,angle_n).spikes(1,:)); %number of bins equal
                                                     %to the # of ms
@@ -39,7 +39,7 @@ plot(xi,f*normalization_factor, 'LineWidth', 2)
 
 %% Hand Trajectory
 angles = trial(1, :).handPos;
-figure(2)
+figure()
 hold on
 for i=1:size(trial,2)
     plot3(trial(1, i).handPos(1, :), trial(1, i).handPos(2, :), trial(1, i).handPos(3, :))
@@ -50,7 +50,7 @@ ylabel('Y')
 zlabel('Z')
 grid on
 
-figure(3)
+figure()
 hold on
 for i=1:size(trial,2)
     plot(trial(1, i).handPos(1, :), trial(1, i).handPos(2, :))
@@ -62,34 +62,32 @@ grid on
 
 %% Tuning curve
 hold off;
-unit_n = 76;
-unit_n = 8;
+pref_directions = zeros(1, size(trial(1,1).spikes, 1));
+for unit_n=1:size(trial, 2)
+    average_freq = zeros(1, size(trial, 2));
+    std = zeros(1, size(trial, 2));
+    angles = [30 70 110 150 190 230 310 350].*(pi/180); %angles corresponding to the 8
 
-average_freq = zeros(1, size(trial, 2));
-std = zeros(1, size(trial, 2));
-angles = [30 70 110 150 190 230 310 350].*(pi/180); %angles corresponding to the 8
-
-for angle_n = 1:size(trial, 2)
-    for i = 1:size(trial, 1)
-        temp = average_freq(1, angle_n);
-        x_new = mean(trial(i,angle_n).spikes(unit_n,:));
-        average_freq(1, angle_n) = average_freq(1, angle_n) + x_new;
-%         std(1, angle_n) = 1/i*((i-1)*std(1, angle_n) + (i-1)*(i-2)*(average_freq(1, angle_n)-temp)^2); %running sample variance
-        std(1, angle_n) = std(1, angle_n) + (x_new-temp)*(x_new-average_freq(1, angle_n)); %running sample intrmediate variance
+    for angle_n = 1:size(trial, 2)
+        for i = 1:size(trial, 1)
+            temp = average_freq(1, angle_n);
+            x_new = mean(trial(i,angle_n).spikes(unit_n,:));
+            average_freq(1, angle_n) = average_freq(1, angle_n) + x_new;
+    %         std(1, angle_n) = 1/i*((i-1)*std(1, angle_n) + (i-1)*(i-2)*(average_freq(1, angle_n)-temp)^2); %running sample variance
+            std(1, angle_n) = std(1, angle_n) + (x_new-temp)*(x_new-average_freq(1, angle_n)); %running sample intrmediate variance
+        end
     end
-end
-average_freq = average_freq/i; %averaging
-% std = sqrt(std); %standard deviation from variance
-std = sqrt(std/i); %standard deviation from intermediate variance
-
+    average_freq = average_freq/i; %averaging
+    % std = sqrt(std); %standard deviation from variance
+    std = sqrt(std/i); %standard deviation from intermediate variance
+    pref_directions(unit_n) = find(average_freq == max(average_freq));
+end 
 %vertical bars representing the firing frequency at the different angles
-figure(3)
+figure()
 hold on
 bar(average_freq)
 errorbar(1:8, average_freq, std/2, std/2)
 
-figure(100)
-
 %firing frequency in polar coordinates
-figure(2)
+figure()
 polarplot([angles;angles], [zeros(size(average_freq));average_freq], 'LineWidth', 3);
