ad6ac42a4696efc9164d3f559c4f83acf4d40712
lm1017 <lm1017@ic.ac.uk>
2021-03-08 09:52:39 +0000

    Correction

--- test_estimator.m
+++ test_estimator.m
@@ -4,7 +4,7 @@ load monkeydata_training
 
 estimator = PositionEstimator();
 
-%% Teting dataset creation function
+%% Testing dataset creation function
 [eeg_train, eeg_test, x_train, x_test] = estimator.getDataset(trial, 1, 80);
 
 %% Testing creation of dynamics matrices
