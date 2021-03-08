96c18b3a05810a2723b8e878d51422ec48916d4c
Lapo Rastrelli <lr4617@ic.ac.uk>
2021-03-08 23:35:08 +0100

    Pulling

--- AngleClassifier.m
+++ AngleClassifier.m
@@ -8,7 +8,7 @@ classdef AngleClassifier
    
         function classified_data = k_nn(~, training_data, labels, test_data, k)
             classified_data = zeros(1, length(test_data));
-            [nearest_neighbours, dist] = knnsearch(training_data, test_data, 'K', k);
+            [nearest_neighbours, ~] = knnsearch(training_data, test_data, 'K', k);
             for i = 1:length(test_data)
                 nn_indices = nearest_neighbours(i, :);
                 nn_labels = labels([nn_indices]);
