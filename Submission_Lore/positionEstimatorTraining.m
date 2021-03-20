ef2c385da5fd16a607e54ccbd716303d96006974
lm1017 <lm1017@ic.ac.uk>
2021-03-20 15:09:02 +0000

    newfolder

---
+++ Submission_Lore/positionEstimatorTraining.m
@@ -0,0 +1,37 @@
+function [modelParameters] = positionEstimatorTraining(training_data)
+  % Arguments:
+ 
+  % - training_data:
+  %     training_data(n,k)              (n = trial id,  k = reaching angle)
+  %     training_data(n,k).trialId      unique number of the trial
+  %     training_data(n,k).spikes(i,t)  (i = neuron id, t = time)
+  %     training_data(n,k).handPos(d,t) (d = dimension [1-3], t = time)
+  
+  % Return Value:
+  
+  % - modelParameters:
+  %     - A
+  %     - W
+  %     - H
+  %     - Q
+  %     - classifier
+  %     - initial parameters
+  
+  
+  processor = Processing();
+  trj = Trajectory();
+  
+  initial_pos = processor.get_data_matrix_initial(training_data);
+  [~, pos] = processor.get_data_matrix(training_data);
+  final_pos = processor.get_data_matrix_final(training_data);
+  
+  start = trj.initial_positions(initial_pos);
+  [avgT, stdT] = trj.averageTrajectory(pos);
+  obj = trj.objective_positions(final_pos);
+  
+  modelParameters.initial = start;
+  modelParameters.traces = avgT;
+  modelParameters.deviation = stdT;
+  modelParameters.objectives = obj;
+  
+end
