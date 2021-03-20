e9f18212b5aa3acf181f0ff6c72c67fe80356ddd
lm1017 <lm1017@ic.ac.uk>
2021-03-20 19:09:01 +0000

    trials

--- Classifier/positionEstimatorTraining.m
+++ Classifier/positionEstimatorTraining.m
@@ -12,12 +12,12 @@ function [modelParameters] = positionEstimatorTraining(training_data)
  
   %silent_neurons = [8 10 24 25 28 38 42 46 49 52 73 74 76 79 83]; %Threshold of 5
   %silent_neurons = [8 10 11 19 20 24 25 28 38 39 42 46 49 52 54 58 62 73 74 76 79 83 84 95]; %Threshold of 10
-  silent_neurons = [2 5 6 8 10 11 19 20 21 23 24 25 26 28 30 35 37 38 39 42 44 46 49 52 53 54 58 59 60 62 63 64 70 73 74 76 79 82 83 84 90 95 97]; %Threshold of 15
-  [clean_trial, clean_neurons] = processor.clean_dataset(training_data, silent_neurons);
+  %silent_neurons = [2 5 6 8 10 11 19 20 21 23 24 25 26 28 30 35 37 38 39 42 44 46 49 52 53 54 58 59 60 62 63 64 70 73 74 76 79 82 83 84 90 95 97]; %Threshold of 15
+  %[clean_trial, clean_neurons] = processor.clean_dataset(training_data, silent_neurons);
 
-  neurons_per_angle = 6;
-  [active_neurons active_neurons_matrix] = processor.mostActive(clean_trial, neurons_per_angle, clean_neurons);
-  %[active_neurons active_neurons_matrix] = processor.mostActive(training_data, neurons_per_angle, 1:98);
+  neurons_per_angle = 12;
+  %[active_neurons active_neurons_matrix] = processor.mostActive(clean_trial, neurons_per_angle, clean_neurons);
+  [active_neurons active_neurons_matrix] = processor.mostActive(training_data, neurons_per_angle, 1:98);
   eccoli_qui = 1:98; %processor.mostActive(clean_trial, 4, 300, 400);
 
   [samples, labels] = processor.create_dataset(training_data, active_neurons, 320, 1);
